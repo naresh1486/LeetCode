52fa4d2130cc453660237661f0bc6df3e51675ab
naresh1486 <S24CSEU1486@bennett.edu.in>
2026-06-22 21:25:48 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.43%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+
+        string ans = "";
+        int i = s.length() - 1;
+
+        while (i >= 0) {
+
+            // Skip spaces
+            while (i >= 0 && s[i] == ' ') {
+                i--;
+            }
+
+            if (i < 0) {
+                break;
+            }
+
+            int j = i;
+
+            // Find the beginning of the word
+            while (j >= 0 && s[j] != ' ') {
+                j--;
+            }
+
+            // Add space before every word except the first
+            if (!ans.empty()) {
+                ans += ' ';
+            }
+
+            // Append the word
+            ans += s.substr(j + 1, i - j);
+
+            i = j - 1;
+        }
+
+        return ans;
+    }
+};
